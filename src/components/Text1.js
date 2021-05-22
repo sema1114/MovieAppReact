841b6e630c7172ab59b19aebcf039ddcfd953eec
Sema Kaya <semabayri00@gmail.com>
2021-05-22 22:35:53 +0200

    First

---
+++ src/components/Text1.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Text1 extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Text1 componenet</h2>
+            </div>
+        )
+    }
+}
