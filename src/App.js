b4a60ac1036d02e3976379f0af9cd226b0f6ab84
Sema Kaya <semabayri00@gmail.com>
2021-05-27 11:11:28 +0200

    Complated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React,{Component} from 'react';
-import Text1 from './components/Text1';
+import './App.css';
+
+import { Provider } from 'react-redux';
+
+import {HashRouter as Router,Route} from 'react-router-dom'
+
+
+
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+
+import Landing from './components/home/Landing';
+import Movie from './components/home/Movie';
+
+
+import store from './store';
 
 export default class App extends Component {
 render(){
   return (
-    <div>
-      <Text1/>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <div>
+      <Navbar />
+      <Route exact path="/" component={Landing}/>
+      <Route exact path="/movie/:id" component={Movie}/>
+
+      <Footer/>
+      </div>
+      </Router>
+      </Provider>
   );
 
 }
